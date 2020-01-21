0ce538f0d26ec794432e0aa61963534ccf229b1b
Christian Montero Martínez <christianmontero08@gmail.com>
2020-01-20 23:28:13 -0800

    chat component change display to flexbox

--- src/components/chats/chats.component.js
+++ src/components/chats/chats.component.js
@@ -20,6 +20,49 @@ class ChatsComponent extends Component{
             <div className="chat-component-container">
                 <div className="my-contacts">
                     <h3>My Contacts</h3>
+                        {
+                            ((!this.props.currentuser.contacts) || (this.props.currentuser.contacts.length === 0)) ? (
+                                <h4>You don't have any contacts</h4> 
+                            ):(
+                                this.props.currentuser.contacts.map( contact => {
+                                    return(
+                                        <div key={contact._id} className="contact-container">
+                                            <h5>{contact.firstname + " " + contact.lastname}</h5>
+                                            <h5>{contact.email}</h5>
+                                        </div>
+                                    )
+                                })
+                            )
+                        }
+                </div>
+                <div className="chat-component">
+                    <div className="chat-content">
+                        
+                    </div>
+                    <div className="input-container">
+                        <input className="message-input" type="text"/>
+                        <input className="button-input" type="button" value="send"/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    const { loggedIn } = state;
+    if(loggedIn)
+        console.log("Current user is: ", loggedIn);
+    return { 
+        currentuser: (loggedIn) ? loggedIn : null 
+    }
+}
+
+export default connect(mapStateToProps, null)(ChatsComponent);
+
+/*
+ <div className="my-contacts">
+                    <h3>My Contacts</h3>
                     {
                         ((!this.props.currentuser.contacts) || (this.props.currentuser.contacts.length === 0)) ? (
                             <h4>You don't have any contacts</h4> 
@@ -44,18 +87,71 @@ class ChatsComponent extends Component{
                             <input className="button-input" type="button" value="send"/>
                         </div>
                     </div>
-            </div>
-        )
-    }
+
+                    .chat-component-container{
+    display: inline-block;
+    width: 100%;
+    box-sizing: content-box;
 }
 
-function mapStateToProps(state){
-    const { loggedIn } = state;
-    if(loggedIn)
-        console.log("Current user is: ", loggedIn);
-    return { 
-        currentuser: (loggedIn) ? loggedIn : null 
-    }
+.my-contacts{
+    border: 1px solid #eaeef0;
+    height: 100%;
+    position: fixed;
+    text-align: center;
+    border-collapse: collapse;
+    display: inline-block;
 }
 
-export default connect(mapStateToProps, null)(ChatsComponent);
+.contact-container{
+    border: 1px solid #eaeef0;
+    text-align: left;
+    padding: 0 15px;
+    cursor: pointer;
+    border-collapse: collapse;
+}
+
+.contact-container h5{
+    margin: 7px 0;
+}
+
+.chat-component{
+    display: block;
+    margin-left: 150px;
+    background-color: #e6e8fa;
+    width: 100%;
+    box-sizing: border-box;
+    position: fixed;
+    height: 100%;
+}
+
+.chat-content{
+    margin: 10px;
+    width: 77%;
+    height: 70%;
+    background-color: #fff;
+    display: block !important;
+}
+
+.input-container{
+    display: block !important;
+    margin: 10px;
+    height: 11%;
+}
+
+.input-container .message-input{
+    margin: 5px;
+    height: 90%;
+    width: 70%;
+    box-sizing: border-box;
+    display: inline-block;
+}
+
+.input-container .button-input{
+    width: 8%;
+    display: inline-block;
+    background-color:#0d98ba;
+    color: #fff;
+    padding: 29px;
+}
+*/
